fe45fed696ac859617ff8d936b0e62abc64497ab
Skareeg <skareeg@gmail.com>
2022-08-09 17:14:22 +0000

    Notes and small lib adjustments.

---
+++ src/basic.rs
@@ -0,0 +1,4 @@
+//! # Basic electrical formulas and conventions.
+//! 
+//! This is going to hold a series of functions and structure for basic electrical simulations.
+//! See the notes area for more information.
